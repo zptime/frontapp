db4e81f9e831b1c927f489220f6e4f03f8914565
zhangpian <1063764688@qq.com>
2019-12-08 22:17:58 +0800

    init project

---
+++ src/page/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="contentBox" v-cloak>
+        <div class="home">
+            <h1>{{ msg }}</h1>
+            <h2>Essential Links</h2>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script type="es6">
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
